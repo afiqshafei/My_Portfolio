1fe860ae37dd8ec47254b090ae1a16b367863743
Afiq Shafei <afiq.shafei@gmail.com>
2025-04-05 23:17:47 +0800

    Updated index.html and styles.css files

--- scripts.js
+++ scripts.js
@@ -1,14 +1,6 @@
-const header = document.getElementById("header");
-header.textContent = "Welcome to the Updated DOM Manipulation Lab .V2";
-const paragraph = document.querySelector("#intro");
-paragraph.textContent = "This paragraph have been changed";
-header.style.color = "blue";
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu').querySelector('ul');
 
-const input = document.getElementById("userInput");
-const submitButton = document.getElementById("submitButton");
-
-submitButton.addEventListener('click', function() {
-	const userInputText = input.value;
-paragraph.textContent = `User entered: ${userInputText}`;
-	console.log(userInputText);
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
 });
